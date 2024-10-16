c3801c7bd4e733c02ff96f4303dbc32bf924da5e
rohitcod3 <rohit.bangar9@gmail.com>
2024-10-17 05:00:07 +0530

    redux-commit

--- frontend/src/redux/store.jsx
+++ frontend/src/redux/store.jsx
@@ -1,9 +1,9 @@
 import {configureStore} from "@reduxjs/toolkit";
-import { userReducer } from "./userSlice";
-const store = configureStore({
+import  userReducer  from "./userSlice.jsx";
+export const store = configureStore({
     reducer:{
       user:userReducer
     }
-})
+});
 
 export default store;
